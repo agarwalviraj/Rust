af6a225441c72fd12a3345f9f9412ecc2a328d08
agarwalviraj <viraj.ag007@gmail.com>
2021-07-17 22:12:55 +0530

    operator overloading

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-trait Summbale<T>{
-    fn sum(&self)->T;
-}
+use std::ops::Add;
 
-impl Summbale<i32> for Vec<i32>{
-    fn sum(&self)->i32 {
-        let mut sum =0;
-        for i in self{
-            sum+=*i;
-        }    
-        sum
+#[derive(Debug)]
+struct Point {
+    x:f64, 
+    y:f64
+}
+impl Add for Point {
+    type Output=Point;
+    fn add(self, other: Self) -> Self::Output {
+        Point{
+            x:self.x+other.x,
+            y:self.y+other.y
+        }
     }
 }
-
-
-
 fn main(){
-    let a = vec![1,2,3,4,5];
-    println!("{}", a.sum());
+    let p1 =Point{x:1.5, y:2.4};
+    let p2 =Point{x:8.5, y:0.4};
+    let p3 =p1+p2;
+    println!("{:?}", p3)
 }
