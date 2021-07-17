3642c0d92556d0661b9b7f420eb55d4b84b63cda
agarwalviraj <viraj.ag007@gmail.com>
2021-07-17 14:45:37 +0530

    generic traits

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,29 @@
-struct RustDev{
-    awesome :bool
-}
- struct JavaDev{
-     awesome:bool
- }
 
- trait Developer{
-    fn new(awesome:bool) -> Self;
-    fn language(&self) -> &str;
-    fn say_hello(&self) {println!("Hello world!")}
- }
-
- impl Developer for RustDev{
-    fn new(awesome:bool) -> Self {
-        RustDev{awesome:awesome}
-    }
-
-    fn language(&self) -> &str {
-        "Rust"
-
-    }
-
-    fn say_hello(&self) {
-        println!("println!(\"Hello world!\");");
-        
-    }
+trait Bark{
+    fn bark(&self) -> String;
 }
 
-impl  Developer for JavaDev {
-    fn new(awesome:bool) -> Self {
-        JavaDev{awesome:awesome}
-    }
-
-    fn language(&self) -> &str {
-        "Java 1.8"
-    }
-
-    fn say_hello(&self) {println!("System.out.println(\"Hello world !\");")}
+struct Dog{
+    species: &'static str
 }
- fn main(){
-    // let r = RustDev{awesome:true};
-    let r = RustDev::new(true);
-    let j = JavaDev::new(false);
-    println!("{}",r.language());
-    r.say_hello();
-    println!("{}",j.language());
-    j.say_hello()
- }
+ 
+struct  Cat{
+    color:&'static str
+}
+
+impl Bark for Dog {
+   fn bark(&self) -> String {
+       return format!("{} barking", self.species)
+   } 
+}
+
+fn bark_it<T:Bark>(b:T){
+    println!("{}", b.bark())
+}
+
+fn main(){
+    let dog=Dog{species:"retreiver"};
+    let cat=Cat{color:"black"};
+     bark_it(dog);
+
+}
