7d1de3a4dc767243b1dda7c1dcb25d044f7a1e25
agarwalviraj <viraj.ag007@gmail.com>
2021-07-17 21:54:28 +0530

    returning traits

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
-
-trait Bark{
-    fn bark(&self) -> String;
-}
-
 struct Dog{
-    species: &'static str
+
 }
- 
-struct  Cat{
-    color:&'static str
+struct Cat{
+
 }
 
-impl Bark for Dog {
-   fn bark(&self) -> String {
-       return format!("{} barking", self.species)
-   } 
+trait Animal{
+    fn make_noise(&self)->&'static str;
 }
 
-fn bark_it<T:Bark>(b:T){
-    println!("{}", b.bark())
+impl Animal for Dog {
+    fn make_noise(&self)->&'static str {
+        return "woof";
+    }
+}
+
+impl Animal for Cat {
+    fn make_noise(&self)->&'static str {
+        return "meow";
+    }
+}
+
+fn get_animal(rand_num:f64)->Box <dyn Animal>{
+    if rand_num<1.0{
+         Box::new(Dog{})
+    }else{
+         Box::new(Cat{})
+    }
 }
 
 fn main(){
-    let dog=Dog{species:"retreiver"};
-    let cat=Cat{color:"black"};
-     bark_it(dog);
+  println!("The aimal says {}",get_animal(0.5).make_noise()) ;
+  println!("The aimal says {}",get_animal(1.5).make_noise()) 
 
 }
