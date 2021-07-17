2836bef3d1c07078fa9572fe8bf794ff22084632
agarwalviraj <viraj.ag007@gmail.com>
2021-07-17 22:47:58 +0530

    static dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-use std::ops::Add;
-
-#[derive(Debug)]
-struct Point {
-    x:f64, 
-    y:f64
+trait Duplicateable{
+    fn dupl(&self)->String;
 }
-impl Add for Point {
-    type Output=Point;
-    fn add(self, other: Self) -> Self::Output {
-        Point{
-            x:self.x+other.x,
-            y:self.y+other.y
-        }
+impl Duplicateable for String{
+    fn dupl(&self)->String {
+        format!("{0}{0}",*self)
     }
 }
+
+impl Duplicateable for i32 {
+    fn dupl(&self)->String {
+        format!("{}",*self*2)
+    }
+}
+
+fn duplicate <T:Duplicateable> (x:T){
+    println!("{}",x.dupl());
+}
+
 fn main(){
-    let p1 =Point{x:1.5, y:2.4};
-    let p2 =Point{x:8.5, y:0.4};
-    let p3 =p1+p2;
-    println!("{:?}", p3)
+    let a =42;
+    let b ="Hi jhon".to_string();
+    duplicate(a);
+    duplicate(b);
 }
